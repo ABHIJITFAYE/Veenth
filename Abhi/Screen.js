63f46c46dc1cfbafa4fa6d51d65460435ef5e84e
ABHIJITFAYE <fayeabhijit28@gmail.com>
2023-01-21 13:08:46 +0530

    4th commits

--- Abhi/Screen.js
+++ Abhi/Screen.js
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity,} from 'react-native'
 import React from 'react'
 
 const Screen = () => {
   return (
     <View>
       <Text>Hello World</Text>
+<TouchableOpacity>
+      <Text> World </Text></TouchableOpacity>
+      <Text>Message</Text>
+      
     </View>
   )
 }
