297b166bf9f35f3b131f519acd2076bc8a9b7135
ABHIJITFAYE <fayeabhijit28@gmail.com>
2023-01-21 12:58:05 +0530

    3th commits

--- Abhi/Screen.js
+++ Abhi/Screen.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function HELLO () {
+const Screen = () => {
   return (
     <View>
-      <Text>HELLO </Text>
+      <Text>Hello World</Text>
     </View>
   )
 }
+
+export default Screen;
--- Abhi/app.js
+++ Abhi/app.js
@@ -1,1 +1,14 @@
-app.js
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class app extends Component {
+  render() {
+    return (
+      <View>
+        <Text>app</Text>
+      </View>
+    )
+  }
+}
+
+
