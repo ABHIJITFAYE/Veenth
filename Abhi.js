d2aafddb49e3b9393657f0f482538e03f248d6e8
ABHIJITFAYE <fayeabhijit28@gmail.com>
2023-01-21 13:14:28 +0530

    5th commits

---
+++ Abhi.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View ,Pressable} from 'react-native'
+import React from 'react'
+
+export default function Abhi() {
+  return (
+    <View>
+        <Pressable>
+      <Text>Abhi</Text></Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
